42e5cd118c70243728d37d13288358652c856ef2
pavelusov <ipavelusov@gmail.com>
2017-09-29 23:09:02 +0500

    Added animations

--- app/js/sections/controllers.js
+++ app/js/sections/controllers.js
@@ -1,20 +1,36 @@
 import {module} from './module';
 
-module.controller('sectionsController', ($scope, $element) => {
-    let leftColumn = $element[0].querySelector('.leftColumnSection'),
+module.controller('sectionsController', ($scope, $element, $timeout) => {
+    const leftColumn = $element[0].querySelector('.leftColumnSection'),
         rightColumn = $element[0].querySelector('.rightColumnSection');
 
     $scope.sections = ['intro'];
-    $scope.sectionTitle = ' ';
+    $scope.sectionTitle = '';
+    $scope.isActiveContent = false;
+    $scope.transitionContent = () => {
+        $scope.isActiveContent = !$scope.isActiveContent;
+    };
 
     $scope.$on('sectionContent', (event, section) => {
+        const changeContent = () => {
+            $scope.transitionContent();
+            if (angular.isElement(leftColumn) && angular.isElement(rightColumn)) {
+                $scope.sectionTitle = section.title;
+                leftColumn.innerHTML = section.html;
+                rightColumn.innerHTML = section.rules;
+            }
+        };
 
-        if (angular.isElement(leftColumn) && angular.isElement(rightColumn)) {
-            $scope.sectionTitle = section.title;
-            leftColumn.innerHTML = section.html;
-            rightColumn.innerHTML = section.rules;
-            $scope.sections.pop();
-            $scope.sections.push('section');
+        $scope.sections.pop();
+        $scope.sections.push('section');
+
+        if (!$scope.isActiveContent) {
+            changeContent();
+        } else {
+            $scope.transitionContent();
+            $timeout(() => {
+                changeContent();
+            }, 800);
         }
     });
     $scope.checkShowSection = (section) => {
