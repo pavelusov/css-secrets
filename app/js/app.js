a68a9b9843dc08089d6ace8f812e29ff574f2838
pavelusov <ipavelusov@gmail.com>
2017-09-30 22:28:59 +0500

    Created module: modal. Added button into main page

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,15 @@
 import {module as utils} from './utils';
 import {module as buttons} from './buttons';
 import {module as sections} from './sections';
+import {module as modals} from './modals';
 
 import '../less/style.less';
 
 let app = angular.module('companyWebsite', ['ngAnimate',
     utils.name,
     buttons.name,
-    sections.name
+    sections.name,
+    modals.name
 ]);
 
 export {app};
---
+++ app/js/modals/directives.js
@@ -0,0 +1,13 @@
+import {module} from './module';
+
+const questionTemplate = require('./templates/question.html');
+
+module.directive('modalFormQuestion', () => {
+    return {
+        restrict: 'E',
+        templateUrl: questionTemplate,
+        controller: ($scope, $http) => {
+
+        }
+    };
+});
